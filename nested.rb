5d3ae914de948abae13d4031e60b689064195f49
Carrie Savinell <carrielane89@gmail.com>
2018-05-15 13:46:18 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -87,8 +87,8 @@ def adding_matz
         }
      }
 
-programmer_hash[:yukihiro_matsumoto] 
-puts programmer_hash
+programmer_hash[:yukihiro_matsumoto] = [:known_for] = "Ruby"
+programmer_hash
 
 
 end
