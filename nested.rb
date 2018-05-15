0015ecf6adbff9c53aeb6c9b8fdab069911f1a8e
Carrie Savinell <carrielane89@gmail.com>
2018-05-15 13:44:18 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -66,7 +66,6 @@ end
 
 
 def adding_matz
-# add the following information to the top level of programmer_hash
 # :yukihiro_matsumoto => {
 #   :known_for => "Ruby",
 #    :languages => ["LISP", "C"]
@@ -88,7 +87,7 @@ def adding_matz
         }
      }
 
-programmer_hash << [:yukihiro_matsumoto] 
+programmer_hash[:yukihiro_matsumoto] 
 programmer_hash
 
 
