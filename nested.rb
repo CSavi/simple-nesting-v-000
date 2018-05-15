c0f75365450147def1b7191cc13544aee8e3a5b1
Carrie Savinell <carrielane89@gmail.com>
2018-05-15 14:23:18 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -132,6 +132,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-programmer_hash[:dennis_ritchie][:languages] = "Assemby"
+programmer_hash[:dennis_ritchie][:languages] <<"Assemby"
 programmer_hash
 end
